08bdd978bbbf10be71b86966b0a2fc64b8b753a0
latitude 3470 <latitude 3470@dell>
2019-01-28 19:00:46 +0200

    First software Homework

---
+++ src/SoftHw1.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class SoftHw1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null,"Hello ,My Name is Haya Barakat");
+		System.exit(0);
+	}
+
+}
